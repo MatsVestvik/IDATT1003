4a17e92ff54374036a30ee21f7355c108fde9054
Mats Orpia Vestvik <matsov@stud.ntnu.no>
2025-10-28 19:01:10 +0100

    added extra functionallity

--- Øv10.java/Oppg2/Test.java
+++ Øv10.java/Oppg2/Test.java
@@ -27,7 +27,10 @@ public class Test{
                     1. Print Register
                     2. Find all type
                     3. Find all in price
-                    4. exit
+                    4. Add Dish
+                    5. Add new Menu
+                    6. Add dish to menu
+                    7. exit
                     """);
 
             int choice = scanner.nextInt();
@@ -36,7 +39,10 @@ public class Test{
                 case 1 -> register.printRegister();
                 case 2 -> case2();
                 case 3 -> case3();
-                case 4 -> running = false;
+                case 4 -> case4();
+                case 5 -> case5();
+                case 6 -> case6();
+                case 7 -> running = false;
                 default -> {}
             }
         } 
@@ -45,7 +51,7 @@ public class Test{
     }
 
     public static void case2(){
-        System.out.println("Enter a type (Dinner, Dessert...):");
+        System.out.print("Enter a type (Dinner, Dessert...): ");
         scanner = new Scanner(System.in);
         String type = scanner.nextLine();
 
@@ -60,5 +66,36 @@ public class Test{
         double max = scanner.nextDouble();
 
         register.finnMenyerInnenforPrisintervall(min, max);
+    }
+    public static void case4(){
+        scanner = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter Type: ");
+        String type = scanner.nextLine();
+        System.out.print("Enter reciepie: ");
+        String reciepe = scanner.nextLine();
+        System.out.print("Enter price: ");
+        double price = scanner.nextDouble();
+
+        register.addDish(name, type, reciepe, price);
+    }
+
+    public static void case5(){
+        scanner = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine();
+
+        register.addMeny(name);
+    }
+
+    public static void case6(){
+        scanner = new Scanner(System.in);
+        System.out.print("Enter menu: ");
+        String menu = scanner.nextLine();
+        System.out.print("Enter Dish: ");
+        String dish = scanner.nextLine();
+        register.addDTM(menu, dish);
+        
     }
 } 
